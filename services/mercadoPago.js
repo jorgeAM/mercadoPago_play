b9a201fd4283d14c950399bb6297e13c4e501268
Jorge L <jorge.alfmur@gmail.com>
2019-06-26 04:41:35 -0500

    hide accesToken

--- services/mercadoPago.js
+++ services/mercadoPago.js
@@ -1,6 +1,6 @@
 const mercadopago = require('mercadopago')
 
-mercadopago.configurations.setAccessToken('TEST-6017328048569435-061815-44910917ac4a52391d3e41f25bc6f394-445147303')
+mercadopago.configurations.setAccessToken('xxx')
 
 const createPayment =  async data => {
     return mercadopago.payment.save(data)
