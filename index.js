9492f37a6f1448b9d828411ee7decf5b534eb0ed
Jorge L <jorge.alfmur@gmail.com>
2019-06-26 01:00:20 -0500

    connect with mongo

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const bodyParser = require('body-parser');
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const db = mongoose.connection
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost/idPay'
+
+mongoose.connect(mongoURL, {useNewUrlParser: true});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json({message: 'hola mundo'}))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    db.on('error', console.error.bind(console, 'something got wrong! 💀'));
+    db.once('open', function() {
+        console.log('successful connection 🚀')
+        console.log(`server is  listening on port ${port}! 🚀 `)
+    })
+})
